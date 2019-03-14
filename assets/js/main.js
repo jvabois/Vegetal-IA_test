b4d7809dfdf7e31c7d8a770c36618e2590c2bc44
jerome <jeromevab@gmail.com>
2019-03-14 16:14:45 +0100

    UPDATE gauges

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,4 +74,15 @@
 
 		}
 
+	// Gauges.
+		setInterval(function() {
+			var newVal = Math.floor((Math.random() * 179) + 1);
+		
+			$('.gauge--3 .semi-circle--mask').attr({
+			style: '-webkit-transform: rotate(' + newVal + 'deg);' +
+			'-moz-transform: rotate(' + newVal + 'deg);' +
+			'transform: rotate(' + newVal + 'deg);'
+			});				
+		}, 1000);
+
 })(jQuery);
