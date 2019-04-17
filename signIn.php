2a0c552cc0efefd73cb962b6f6f6603216cd8b30
jerome <jeromevab@gmail.com>
2019-04-17 17:21:03 +0200

    UPDATE connexion

--- signIn.php
+++ signIn.php
@@ -1,74 +1,82 @@
-<?php
-session_start();
-$titre="Connexion";
-include("includes/identifiants.php");
-include("includes/functions.php");
-include("includes/constants.php");
-echo '<p><i>Vous êtes ici</i> : <a href="./index.html">Index du forum</a> --> Connexion';
-?>
+<!DOCTYPE HTML>
 
-<?php
-echo '<h1>Connexion</h1>';
-if ($id!=0) erreur(ERR_IS_CO); //verification si déjà connecté ou non
-?>
+<html>
+	<?php
+	session_start();
+	$titre="Connexion";
+	include("includes/identifiants.php");
+	include("includes/functions.php");
+	include("includes/constants.php");
+	echo '<p><i>Vous êtes ici</i> : <a href="./index.html">Index du forum</a> --> Connexion';
+	?>
+ <head>
+  <title>Test PHP</title>
+ </head>
+<body>
+	<?php
+	echo '<h1>Connexion</h1>';
+	if ($id!=0) erreur(ERR_IS_CO); //verification si déjà connecté ou non
+	?>
 
-<?php
-if (!isset($_POST['pseudo'])) //On verifie si le champs pseudo a été rempli
-{
-	echo '<form method="post" action="connexion.php">
-	<fieldset>
-	<legend>Connexion</legend>
-	<p>
-	<label for="pseudo">Pseudo :</label><input name="pseudo" type="text" id="pseudo" /><br />
-	<label for="password">Mot de Passe :</label><input type="password" name="password" id="password" />
-	</p>
-	</fieldset>
-	<p><input type="submit" value="Connexion" /></p></form>
-	<a href="./signUP.html">Pas encore inscrit ?</a>
-	 
-	</div>
-	</body>
-	</html>';
-}
-
-else
-{
-    $message='';
-    if (empty($_POST['pseudo']) || empty($_POST['password']) ) //Oublie d'un champ
-    {
-        $message = '<p>une erreur s\'est produite pendant votre identification.
-	Vous devez remplir tous les champs</p>
-	<p>Cliquez <a href="./connexion.php">ici</a> pour revenir</p>';
-    }
-    else //On check le mot de passe
-    {
-        $query=$db->prepare('SELECT membre_mdp, membre_id, membre_rang, membre_pseudo
-        FROM forum_membres WHERE membre_pseudo = :pseudo');
-        $query->bindValue(':pseudo',$_POST['pseudo'], PDO::PARAM_STR);
-        $query->execute();
-        $data=$query->fetch();
-	if ($data['membre_mdp'] == md5($_POST['password'])) // Acces OK !
+	<?php
+	if (!isset($_POST['pseudo'])) //On verifie si le champs pseudo a été rempli
 	{
-	    $_SESSION['pseudo'] = $data['membre_pseudo'];
-	    $_SESSION['level'] = $data['membre_rang'];
-	    $_SESSION['id'] = $data['membre_id'];
-	    $message = '<p>Bienvenue '.$data['membre_pseudo'].', 
-			vous êtes maintenant connecté!</p>
-			<p>Cliquez <a href="./index.php">ici</a> 
-			pour revenir à la page d accueil</p>';  
+		echo '<form method="post" action="connexion.php">
+		<fieldset>
+		<legend>Connexion</legend>
+		<p>
+		<label for="pseudo">Pseudo :</label><input name="pseudo" type="text" id="pseudo" /><br />
+		<label for="password">Mot de Passe :</label><input type="password" name="password" id="password" />
+		</p>
+		</fieldset>
+		<p><input type="submit" value="Connexion" /></p></form>
+		<a href="./signUP.html">Pas encore inscrit ?</a>
+		
+		</div>
+		</body>
+		</html>';
 	}
-	else // Acces pas OK !
-	{
-	    $message = '<p>Une erreur s\'est produite 
-	    pendant votre identification.<br /> Le mot de passe ou le pseudo 
-            entré n\'est pas correcte.</p><p>Cliquez <a href="./signIn.php">ici</a> 
-	    pour revenir à la page précédente
-	    <br /><br />Cliquez <a href="./index.html">ici</a> 
-	    pour revenir à la page d accueil</p>';
-	}
-    $query->CloseCursor();
-    }
-    echo $message.'</div></body></html>';
 
-}
-?>
+	else
+	{
+		$message='';
+		if (empty($_POST['pseudo']) || empty($_POST['password']) ) //Oublie d'un champ
+		{
+			$message = '<p>une erreur s\'est produite pendant votre identification.
+		Vous devez remplir tous les champs</p>
+		<p>Cliquez <a href="./connexion.php">ici</a> pour revenir</p>';
+		}
+		else //On check le mot de passe
+		{
+			$query=$db->prepare('SELECT membre_mdp, membre_id, membre_rang, membre_pseudo
+			FROM forum_membres WHERE membre_pseudo = :pseudo');
+			$query->bindValue(':pseudo',$_POST['pseudo'], PDO::PARAM_STR);
+			$query->execute();
+			$data=$query->fetch();
+		if ($data['membre_mdp'] == md5($_POST['password'])) // Acces OK !
+		{
+			$_SESSION['pseudo'] = $data['membre_pseudo'];
+			$_SESSION['level'] = $data['membre_rang'];
+			$_SESSION['id'] = $data['membre_id'];
+			$message = '<p>Bienvenue '.$data['membre_pseudo'].', 
+				vous êtes maintenant connecté!</p>
+				<p>Cliquez <a href="./index.php">ici</a> 
+				pour revenir à la page d accueil</p>';  
+		}
+		else // Acces pas OK !
+		{
+			$message = '<p>Une erreur s\'est produite 
+			pendant votre identification.<br /> Le mot de passe ou le pseudo 
+				entré n\'est pas correcte.</p><p>Cliquez <a href="./signIn.php">ici</a> 
+			pour revenir à la page précédente
+			<br /><br />Cliquez <a href="./index.html">ici</a> 
+			pour revenir à la page d accueil</p>';
+		}
+		$query->CloseCursor();
+		}
+		echo $message.'</div></body></html>';
+
+	}
+	?>
+</body>
+</html>
