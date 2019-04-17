c631cbb3fa261a78daccebee07c9cbe6f7db9ff0
jerome <jeromevab@gmail.com>
2019-04-17 17:12:40 +0200

    UPDATE formaulaire connexion

--- functions.php
+++ functions.php
@@ -3,6 +3,6 @@ function erreur($err='')
 {
    $mess=($err!='')? $err:'Une erreur inconnue s\'est produite';
    exit('<p>'.$mess.'</p>
-   <p>Cliquez <a href="./index.php">ici</a> pour revenir à la page d\'accueil</p></div></body></html>');
+   <p>Cliquez <a href="./index.html">ici</a> pour revenir à la page d\'accueil</p></div></body></html>');
 } //arrêt du php + message d'erreur
 ?>
