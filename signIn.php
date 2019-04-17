a7228483fcf0258c3830b48b33ca7035428a47ac
jerome <jeromevab@gmail.com>
2019-04-17 16:11:26 +0200

    UPDATE inscription en php

--- signIn.php
+++ signIn.php
@@ -1,5 +1,15 @@
 <!DOCTYPE HTML>
 
+<?php
+session_start();
+$titre="Connexion";
+include("includes/identifiants.php");
+include("includes/debut.php");
+include("includes/menu.php");
+echo '<p><i>Vous êtes ici</i> : <a href="./index.php">Index du forum</a> --> Connexion';
+?>
+
+
 <html>
 	<head>
 		<title>vegetal'IA</title>
